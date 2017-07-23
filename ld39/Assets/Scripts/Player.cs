1ab8beccf3fda5c07e17e907901d0153dc95dcd8
Terry Gruenewald <tgruenewald@users.noreply.github.com>
2017-07-23 14:22:20 -0600

    Parallax (#1)
    * scrolling sky and ground work
    
    * parallax
    
    * saved proj
    
    * proj settings

---
+++ ld39/Assets/Scripts/Player.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour {
+	private float move;
+	private bool jump;
+	public float maxSpeed = 3.0f;
+	public float jumpForce = 20f;
+	public bool grounded = true;
+    public LayerMask whatIsGround;
+	private CircleCollider2D groundCheck;
+	private Animator animator;
+	private bool facingRight = true;
+	// Use this for initialization
+	void Start () {
+		groundCheck = GetComponent<CircleCollider2D>();
+//		animator = GetComponent<Animator>();
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		grounded = groundCheck.IsTouchingLayers (whatIsGround);
+		move = Input.GetAxis ("Horizontal");
+		if (move > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (move < 0 && facingRight)
+        {
+            Flip();
+        }
+//		if (move != 0) {
+//			animator.SetBool("isMoving", true);
+//		}
+//		else {
+//			animator.SetBool("isMoving", false);
+//		}
+
+		jump = Input.GetButtonDown ("Jump") || Input.GetButtonDown ("Vertical");
+		
+		//Debug.Log ("move = " + move);
+	}
+	void FixedUpdate () {
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (move * maxSpeed, GetComponent<Rigidbody2D> ().velocity.y);	
+
+		if (grounded && jump) {
+			GetComponent<Rigidbody2D> ().AddForce(new Vector2(0f, jumpForce));
+		}
+	}
+	void OnTriggerEnter2D(Collider2D coll) {
+//		if (coll.gameObject.tag == "spikes") {
+//			// reload the scene
+//			SceneManager.LoadScene( SceneManager.GetActiveScene().name); 
+//
+//		}
+//		if (coll.gameObject.tag == "log") {
+//			// reload the scene
+//			SceneManager.LoadScene( "scene2"); 
+//
+//		}		
+	}
+    void Flip()
+    {
+        //Debug.Log("switching...");
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }	
+}
---
+++ ld39/Assets/Scripts/ScrollingBackground.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// https://www.youtube.com/watch?v=QkisHNmcK7Y
+public class ScrollingBackground : MonoBehaviour {
+
+	public float backgroundSize;
+	public float parallaxSpeed;
+	public bool scrolling, parallax;
+
+	private Transform cameraTransform;
+	private Transform[] layers;
+	private float viewZone = 10;
+	private int leftIndex;
+	private int rightIndex;
+	private float lastCamaraX;
+
+
+
+	private void Start() {
+		cameraTransform = Camera.main.transform;
+		lastCamaraX = cameraTransform.position.x;
+		layers = new Transform[transform.childCount];
+		for (int i = 0; i < transform.childCount; i++) {
+			layers [i] = transform.GetChild (i);
+		}
+		leftIndex = 0;
+		rightIndex = layers.Length - 1;
+	}
+
+	private void Update() {
+		if (parallax) {
+			float deltaX = cameraTransform.position.x - lastCamaraX;
+			transform.position += Vector3.right * (deltaX * parallaxSpeed);			
+		}
+
+		lastCamaraX = cameraTransform.position.x;
+		if (scrolling) {
+			if (cameraTransform.position.x < (layers[leftIndex].transform.position.x + viewZone)) {
+				ScrollLeft ();
+			}
+			if (cameraTransform.position.x > (layers[rightIndex].transform.position.x - viewZone)) {
+				ScrollRight ();
+			}			
+		}
+
+	}
+
+	private void ScrollLeft() {
+		int lastRight = rightIndex;
+		layers [rightIndex].position = Vector3.right * (layers [leftIndex].position.x - backgroundSize);
+		leftIndex = rightIndex;
+		rightIndex--;
+
+		if (rightIndex < 0)
+			rightIndex = layers.Length - 1;
+	}
+	private void ScrollRight() {
+		int lastRight = leftIndex;
+		layers [leftIndex].position = Vector3.right * (layers [rightIndex].position.x + backgroundSize);
+		rightIndex = leftIndex;
+		leftIndex++;
+
+		if (leftIndex == layers.Length)
+			leftIndex = 0;
+	}
+
+}
