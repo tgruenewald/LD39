a03a78eb38974f3f00aeb9a2b5ea9492c0898be7
tgruenewald <terrygruenewald@gmail.com>
2017-07-25 06:48:18 -0600

    made vertical camera adjustment public and made mountain scene conform to player prefab

--- ld39/Assets/Standard Assets/2D/Scripts/Camera2DFollow.cs
+++ ld39/Assets/Standard Assets/2D/Scripts/Camera2DFollow.cs
@@ -10,6 +10,7 @@ namespace UnityStandardAssets._2D
         public float lookAheadFactor = 3;
         public float lookAheadReturnSpeed = 0.5f;
         public float lookAheadMoveThreshold = 0.1f;
+        public float verticalAdjustment = .6f;
 
         private float m_OffsetZ;
         private Vector3 m_LastTargetPosition;
@@ -42,7 +43,7 @@ namespace UnityStandardAssets._2D
                 m_LookAheadPos = Vector3.MoveTowards(m_LookAheadPos, Vector3.zero, Time.deltaTime*lookAheadReturnSpeed);
             }
 
-			Vector3 aheadTargetPos = (target.position + Vector3.up * .6f) + m_LookAheadPos + Vector3.forward*m_OffsetZ;
+			Vector3 aheadTargetPos = (target.position + Vector3.up * verticalAdjustment) + m_LookAheadPos + Vector3.forward*m_OffsetZ;
             Vector3 newPos = Vector3.SmoothDamp(transform.position, aheadTargetPos, ref m_CurrentVelocity, damping);
 
             transform.position = newPos;
