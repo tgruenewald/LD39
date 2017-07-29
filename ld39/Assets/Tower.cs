aaa48b73ef9f518afa3cc8ad071386d4c969833f
thomas2 <thomas2>
2017-07-29 12:55:49 -0600

    the towers have powers

--- ld39/Assets/Tower.cs
+++ ld39/Assets/Tower.cs
@@ -7,8 +7,14 @@ public class Tower : MonoBehaviour {
 	GameObject target = null;
 	public float bulletSpeed = 6f;
 	private bool alreadyFired = false;
+	public int power;
+	public int startpower;
+	public int maxpower;
+	//int powhold;
+	//public GameObject HOLD;
 	// Use this for initialization
 	void Start () {
+		power = startpower;
 		// bullet.transform.position = transform.position;
 	}
 	IEnumerator waitForNextShoot() {
@@ -19,7 +25,12 @@ public class Tower : MonoBehaviour {
 	}
 	// Update is called once per frame
 	void Update () {
-		if (targetList.Count > 0) {
+		if (power <= 0) {
+			Debug.Log ("Reloading");
+		}
+		else{
+			
+				if (targetList.Count > 0) {
 			var grunt = targetList.Peek ();
 			try {
 				if (!grunt.GetComponent<Grunt>().inRange) {
@@ -30,6 +41,7 @@ public class Tower : MonoBehaviour {
 						target = targetList.Dequeue ();
 						alreadyFired = true;
 						StartCoroutine (waitForNextShoot ());
+							power--;
 						GameObject bullet = (GameObject)Instantiate(Resources.Load("prefab/bullet"), GetComponent<Transform>().position, GetComponent<Transform>().rotation) ;
 
 						Debug.Log ("firing");
@@ -44,6 +56,11 @@ public class Tower : MonoBehaviour {
 			}
 
 		}
+		}
+		// (power > maxpower) {
+			//powhold = power - maxpower;
+			//GameObject.Find (HOLD).GetComponent<> ();
+		//}
 
 	}
 	void OnTriggerEnter2D(Collider2D coll){
@@ -52,6 +69,9 @@ public class Tower : MonoBehaviour {
 		    targetList.Enqueue (coll.gameObject);
 			coll.gameObject.GetComponent<Grunt> ().inRange = true;
         }
+		if (coll.gameObject.tag == "wind") {
+			power++;
+		}
 	}
 
 	void OnTriggerExit2D(Collider2D coll){
