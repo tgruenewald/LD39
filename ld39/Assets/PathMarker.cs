74acc9f212526250a64cf8d5cec2c25b765695de
tgruenewald <terrygruenewald@gmail.com>
2017-07-29 08:48:59 -0600

    removing unneeded stock grass

---
+++ ld39/Assets/PathMarker.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathMarker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<SpriteRenderer> ().enabled = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
