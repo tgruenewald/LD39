8bd78c09a3194a0e28c147e0096d1f9ad7d61df4
Jason <kineticpressure@gmail.com>
2017-07-30 16:14:25 -0400

    Added some maps and scenes

---
+++ ld39/Assets/Fortress_power.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fortress_power : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D coll){
+		if (coll == true && coll.gameObject.tag == "Grunt") {
+			Destroy (coll.gameObject, 2.5f);
+			GameObject.Find ("Canvas").GetComponent<GameManager> ().fortressPower -= 100;
+		}
+	}
+}
