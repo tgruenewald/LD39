835d172b243b99aaeba485750919f0a29cd7c3ae
tgruenewald <terrygruenewald@gmail.com>
2017-07-29 21:19:30 -0600

    improved math

--- ld39/Assets/Bullet.cs
+++ ld39/Assets/Bullet.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour {
-
+	public GameObject origTarget;
+	public Vector2 origTargetVelocity;
+	public float speed;
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+		try {
+			if (origTarget != null)	{
+				if (!origTarget.GetComponent<Rigidbody2D>().velocity.Equals(origTargetVelocity)) {
+					GetComponent<Rigidbody2D>().velocity = ShootUtil.firingVector(transform, origTarget, speed);
+					origTargetVelocity = origTarget.GetComponent<Rigidbody2D>().velocity;
+				}
+			}		
+		}
+		catch(MissingReferenceException e) {}
+
+
 	}
 	void OnTriggerEnter2D(Collider2D coll){
 		if (coll.gameObject.tag == "Grunt") {
