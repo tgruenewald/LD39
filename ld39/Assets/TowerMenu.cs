35be3bcd136adfc6a289f19c82c5bf3744da9485
LSM\Carraka <carraka@gmail.com>
2017-07-29 21:26:30 -0400

    trying to get tower menu to work

---
+++ ld39/Assets/TowerMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class TowerMenu : MonoBehaviour {
+
+	public GameObject iconMenuPos;
+	public Vector3 iconMenuPosition;
+	public GameObject autoButton;
+	public GameObject redirectButton;
+
+	// Use this for initialization
+	void Start () {
+		iconMenuPosition = iconMenuPos.transform.position;
+		iconMenuPosition = Camera.main.WorldToScreenPoint (iconMenuPosition);
+
+		//autoButton = GameObject.FindGameObjectWithTag ("AutoButton");
+		//redirectButton = GameObject.FindGameObjectWithTag ("RedirectButton");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseEnter(){
+		Debug.Log ("mouse entered tower collision zone");
+
+		autoButton.gameObject.SetActive (true);
+		redirectButton.gameObject.SetActive (true);
+		GameObject towerMenu = GameObject.FindGameObjectWithTag("tower menu");
+		towerMenu.transform.position = new Vector3 (iconMenuPosition.x, iconMenuPosition.y, transform.position.z);// Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+
+	}
+
+	void OnMouseExit(){
+		autoButton.gameObject.SetActive (false);
+		redirectButton.gameObject.SetActive (false);
+	}
+}
