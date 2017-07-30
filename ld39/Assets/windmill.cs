d94ddfcf4be8eff69d3d89ca22335a39881695ab
thomas2 <thomas2>
2017-07-29 18:04:56 -0600

    addedwindmill

---
+++ ld39/Assets/windmill.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class windmill : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D (Collider2D coll){
+		if (coll.gameObject.tag == "wind") {
+			Debug.Log ("i nedd");
+			GetComponentInParent<Tower> ().power++;
+			Debug.Log ("help");
+		}
+			
+	}
+}
