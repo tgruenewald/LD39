4b6ec63210670b685fb7f5292c23473bbeee45ff
Jason <kineticpressure@gmail.com>
2017-07-30 01:06:25 -0400

    Turret mouse follow and animations and sprites for the turret.

---
+++ ld39/Assets/TurretControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretControl : MonoBehaviour {
+
+
+
+	void Update ()
+	{
+		var mouse = Input.mousePosition;
+		var screenPoint = Camera.main.WorldToScreenPoint(transform.localPosition);
+		var offset = new Vector2(mouse.x - screenPoint.x, mouse.y - screenPoint.y);
+		var angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.Euler(0, 0, angle);
+	}
+}
