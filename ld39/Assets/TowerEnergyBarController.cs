87ada23d38e5b4ed09d58282fa778ebd812ddf99
LSM\Carraka <carraka@gmail.com>
2017-07-30 00:45:53 -0400

    added option to cancel tower placement; added prefab for TowerEnergyBar

---
+++ ld39/Assets/TowerEnergyBarController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TowerEnergyBarController : MonoBehaviour {
+
+	public Slider towerEnergyBar;
+	public int placeholderEnergy = 100;	
+	public Vector3 iconMenuPosition;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void Awake(){
+		towerEnergyBar = GetComponent<Slider> ();
+		transform.position = new Vector3 (iconMenuPosition.x, iconMenuPosition.y, transform.position.z);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		towerEnergyBar.value = placeholderEnergy;
+	}
+}
