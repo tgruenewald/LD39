893ea2e137a5c9702b45258a171b38208ae9f376
tgruenewald <terrygruenewald@gmail.com>
2017-07-31 16:11:54 -0600

    fortress flashes red on damage

--- ld39/Assets/Fortress_power.cs
+++ ld39/Assets/Fortress_power.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Fortress_power : MonoBehaviour {
+	IEnumerator flash() {
+		GetComponent<SpriteRenderer> ().material.SetColor("_Color", Color.red);
+
+		yield return new WaitForSeconds(.1f);
+		GetComponent<SpriteRenderer> ().material.SetColor("_Color", Color.white);
+
+	}
 
 	void OnTriggerEnter2D(Collider2D coll){
 		if (coll == true && coll.gameObject.tag == "Grunt") {
 			Destroy (coll.gameObject, 2.5f);
+			StartCoroutine (flash ());
 			GameObject.Find ("Canvas").GetComponent<GameManager> ().fortressPower -= 100;
 		}
 	}
